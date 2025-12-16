76b2297dcc0028688fec94b66375f2a1448667ef
manjuvaishya69 <manjuvaishya69@gmail.com>
2025-12-16 19:48:44 +0530

    Added theme.kt

---
+++ app/src/main/java/com/dlab/myaipiceditor/ui/theme/color.kt
@@ -0,0 +1,23 @@
+package com.dlab.myaipiceditor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// AI Editor Colors
+val PrimaryBlue = Color(0xFF2196F3)
+val PrimaryBlueDark = Color(0xFF1976D2)
+val SecondaryTeal = Color(0xFF00BCD4)
+val AccentOrange = Color(0xFFFF9800)
+val BackgroundLight = Color(0xFFF5F5F5)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
+val ErrorRed = Color(0xFFE53E3E)
+val SuccessGreen = Color(0xFF38A169)
